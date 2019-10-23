2039a22f24831e4fb9c8d349e792ac57ffa2462f
ITStan <s.stanislavskaya@gmail.com>
2019-10-23 21:57:47 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,7 @@ const counterBlock = document.querySelector('#counter');
 let counter = sessionStorage.getItem('counter') || 0;
 counterBlock.textContent = counter;
 
-
-document.addEventListener('click' , () => {
+document.addEventListener('click', () => {
   counter++;
   counterBlock.textContent = counter;
   sessionStorage.setItem('counter', counter);
