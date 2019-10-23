bc86ce1b5303db335632cc0dace71a77cd2935b7
ITStan <s.stanislavskaya@gmail.com>
2019-10-23 21:56:02 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const counterBlock = document.querySelector('#counter');
+let counter = sessionStorage.getItem('counter') || 0;
+counterBlock.textContent = counter;
+
+
+document.addEventListener('click' , () => {
+  counter++;
+  counterBlock.textContent = counter;
+  sessionStorage.setItem('counter', counter);
+});
